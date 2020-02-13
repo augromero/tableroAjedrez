24c584ae38f569160a6d05e5f8f1eb0c3b2b8c87
Augusto Romero Arango <augusto@sinco.com.co>
2020-02-13 12:55:41 -0500

    Refactor en proyecto de pruebas

--- cli.Tests/TableroAjedrezUnitTest.cs
+++ cli.Tests/TableroAjedrezUnitTest.cs
@@ -5,131 +5,115 @@ using System.Linq;
 namespace cli.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class TableroAjedrezUnitTest
     {
-              
+        private Tablero _tablero;
+        [TestInitialize]
+        public void Inicializar()
+        {
+            _tablero = new Tablero();
+        }
+
         [TestMethod]
         public void Debe_Tablero_Tener64Casillas()
         {
-            Tablero tablero = new Tablero();
-
-            Assert.AreEqual(64, tablero.Casillas.Count);
+            Assert.AreEqual(64, _tablero.Casillas.Count);
         }
 
         [TestMethod]
         public void Debe_PrimeraCasilla_SerA1Blanco()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas.First();
 
-            Casilla casillaA1 = tablero.Casillas.First();
-
-            Assert.AreEqual(Columna.A, casillaA1.Columna);
-            Assert.AreEqual(1, casillaA1.Fila);
-            Assert.AreEqual(Color.BLANCO, casillaA1.Color);
+            Assert.AreEqual(Columna.A, casilla.Columna);
+            Assert.AreEqual(1, casilla.Fila);
+            Assert.AreEqual(Color.BLANCO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_SegundaCasilla_SerB1Negro()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas[1];
 
-            Casilla casillaA1 = tablero.Casillas[1];
-
-            Assert.AreEqual(Columna.B, casillaA1.Columna);
-            Assert.AreEqual(1, casillaA1.Fila);
-            Assert.AreEqual(Color.NEGRO, casillaA1.Color);
+            Assert.AreEqual(Columna.B, casilla.Columna);
+            Assert.AreEqual(1, casilla.Fila);
+            Assert.AreEqual(Color.NEGRO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_TerceraCasilla_SerC1Blanco()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas[2];
 
-            Casilla casillaA1 = tablero.Casillas[2];
-
-            Assert.AreEqual(Columna.C, casillaA1.Columna);
-            Assert.AreEqual(1, casillaA1.Fila);
-            Assert.AreEqual(Color.BLANCO, casillaA1.Color);
+            Assert.AreEqual(Columna.C, casilla.Columna);
+            Assert.AreEqual(1, casilla.Fila);
+            Assert.AreEqual(Color.BLANCO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_OctavaCasilla_SerH1Negro()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas[7];
 
-            Casilla casillaA1 = tablero.Casillas[7];
-
-            Assert.AreEqual(Columna.H, casillaA1.Columna);
-            Assert.AreEqual(1, casillaA1.Fila);
-            Assert.AreEqual(Color.NEGRO, casillaA1.Color);
+            Assert.AreEqual(Columna.H, casilla.Columna);
+            Assert.AreEqual(1, casilla.Fila);
+            Assert.AreEqual(Color.NEGRO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_PrimeraCasillaDeSegundaFual_SerA2Negro()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas[8];
 
-            Casilla casillaA1 = tablero.Casillas[8];
-
-            Assert.AreEqual(Columna.A, casillaA1.Columna);
-            Assert.AreEqual(2, casillaA1.Fila);
-            Assert.AreEqual(Color.NEGRO, casillaA1.Color);
+            Assert.AreEqual(Columna.A, casilla.Columna);
+            Assert.AreEqual(2, casilla.Fila);
+            Assert.AreEqual(Color.NEGRO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_UltimaCasillaDeSegundaFual_SerH2Blanco()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas[15];
 
-            Casilla casillaA1 = tablero.Casillas[15];
-
-            Assert.AreEqual(Columna.H, casillaA1.Columna);
-            Assert.AreEqual(2, casillaA1.Fila);
-            Assert.AreEqual(Color.BLANCO, casillaA1.Color);
+            Assert.AreEqual(Columna.H, casilla.Columna);
+            Assert.AreEqual(2, casilla.Fila);
+            Assert.AreEqual(Color.BLANCO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_PrimeraCasillaDeTerceraFila_SerA3Blanco()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas[16];
 
-            Casilla casillaA1 = tablero.Casillas[16];
-
-            Assert.AreEqual(Columna.A, casillaA1.Columna);
-            Assert.AreEqual(3, casillaA1.Fila);
-            Assert.AreEqual(Color.BLANCO, casillaA1.Color);
+            Assert.AreEqual(Columna.A, casilla.Columna);
+            Assert.AreEqual(3, casilla.Fila);
+            Assert.AreEqual(Color.BLANCO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_UltimaCasillaDeTerceraFila_SerH3Negro()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas[23];
 
-            Casilla casillaA1 = tablero.Casillas[23];
-
-            Assert.AreEqual(Columna.H, casillaA1.Columna);
-            Assert.AreEqual(3, casillaA1.Fila);
-            Assert.AreEqual(Color.NEGRO, casillaA1.Color);
+            Assert.AreEqual(Columna.H, casilla.Columna);
+            Assert.AreEqual(3, casilla.Fila);
+            Assert.AreEqual(Color.NEGRO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_UltimaCasillaDeOctavaFila_SerH3Negro()
         {
-            Tablero tablero = new Tablero();
+            Casilla casilla = _tablero.Casillas[63];
 
-            Casilla casillaA1 = tablero.Casillas[63];
-
-            Assert.AreEqual(Columna.H, casillaA1.Columna);
-            Assert.AreEqual(8, casillaA1.Fila);
-            Assert.AreEqual(Color.BLANCO, casillaA1.Color);
+            Assert.AreEqual(Columna.H, casilla.Columna);
+            Assert.AreEqual(8, casilla.Fila);
+            Assert.AreEqual(Color.BLANCO, casilla.Color);
         }
 
         [TestMethod]
         public void Debe_CuandoSolicitaPorCoordenada_RetornarColor()
         {
-            Tablero tablero = new Tablero();
-
-            Color color = tablero.ObtenerColor(Columna.E, 4);
+            Color color = _tablero.ObtenerColor(Columna.E, 4);
 
             Assert.AreEqual(Color.NEGRO, color);
         }
